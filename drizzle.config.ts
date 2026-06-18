ffaaddcfeeb0a79bd54248f868dc781ae6ba103b
Eman_01 <mirzaeman942@gmail.com>
2026-06-18 23:12:22 +0500

    Change DATABASE_URL to url in dbCredentials

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL ?? '',
+   url: process.env.url ?? '',
   },
   strict: true,
   verbose: true,
