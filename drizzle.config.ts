6e4c2a62207dc35f9c3abd72b292a25b31387905
Eman_01 <mirzaeman942@gmail.com>
2026-06-18 23:14:04 +0500

    Fix environment variable name

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-   url: process.env.url ?? '',
+   url: process.env.URL ?? '',
   },
   strict: true,
   verbose: true,
