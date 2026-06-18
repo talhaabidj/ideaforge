c206ea7c33566c3f4cb41b0ecee45dbd9303435e
Eman_01 <mirzaeman942@gmail.com>
2026-06-18 23:28:02 +0500

    Update database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-   url: process.env.URL ?? '',
+    url: process.env.DATABASE_URL ?? '',
   },
   strict: true,
   verbose: true,
